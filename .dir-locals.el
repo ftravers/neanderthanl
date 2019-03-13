8d874ad17ac2e7f464fd4e18bbfa1e21671ee962
Fenton Travers <fenton.travers@gmail.com>
2019-03-12 23:24:00 -0700

    .

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((cider-default-cljs-repl . figwheel-main)
+         (cider-figwheel-main-default-options . "dev")
+         (cider-clojure-cli-global-options . "-A:be:dev:fe:fe-dev") )))
---
+++ src/nt_dl/basic.clj
@@ -0,0 +1,23 @@
+(ns nt-dl.basic
+  (:require [uncomplicate.commons.core :refer [with-release]]
+            [uncomplicate.neanderthal
+             [native :refer [dv dge]]
+             [core :refer [mv!]]]))
+(with-release [x (dv 0.3 0.9)
+               w1 (dge 4 2 [0.3 0.6
+                            0.1 2.0
+                            0.9 3.7
+                            0.0 1.0]
+                       {:layout :row})
+               h1 (dv 4)]
+  (println (mv! w1 x h1)))
+(with-release [x (dv 0.3 0.9)
+               w1 (dge 4 2 [0.3 0.6
+                            0.1 2.0
+                            0.9 3.7
+                            0.0 1.0]
+                       {:layout :row})
+               h1 (dv 4)
+               w2 (dge 1 4 [0.75 0.15 0.22 0.33])
+               y (dv 1)]
+  (println (mv! w2 (mv! w1 x h1) y)))
